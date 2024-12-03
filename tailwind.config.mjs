dbf711931a3ab09e11f5742c0e69014f59b30667
mertmcd <merttdem@gmail.com>
2024-12-03 20:16:15 +0300

    add Tailwind CSS forms plugin; update PostCSS and TypeScript configurations; remove old Tailwind config

---
+++ tailwind.config.mjs
@@ -0,0 +1,9 @@
+import forms from '@tailwindcss/forms';
+
+export default {
+  content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [forms],
+};
