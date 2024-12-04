0a8ad3eb0fb2d8885d6754a47980ffc6e631b020
mertmcd <merttdem@gmail.com>
2024-12-04 12:55:26 +0300

    update Tailwind CSS configuration; remove old config files; streamline build scripts; enhance PostCSS setup

--- tailwind.config.mjs
+++
@@ -1,9 +0,0 @@
-import forms from '@tailwindcss/forms';
-
-export default {
-  content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
-  theme: {
-    extend: {},
-  },
-  plugins: [forms],
-};
