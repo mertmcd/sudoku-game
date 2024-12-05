8f0853c3520347f2004305dc6f6c3e0bba84cdcf
mertmcd <merttdem@gmail.com>
2024-12-05 13:09:13 +0300

    add ShowHint component; implement hint toggling functionality with remaining hints counter

---
+++ src/components/ShowHint.vue
@@ -0,0 +1,37 @@
+<script lang="ts">
+import { defineComponent, ref } from 'vue';
+
+export default defineComponent({
+  name: 'ShowHint',
+  setup() {
+    const showHint = ref(false);
+    const remainingHints = ref<number>(10);
+
+    const toggleHint = () => {
+      if (remainingHints.value > 0) {
+        showHint.value = !showHint.value;
+        if (showHint.value) remainingHints.value--;
+      }
+    };
+
+    return {
+      showHint,
+      remainingHints,
+      toggleHint,
+    };
+  },
+});
+</script>
+
+<template>
+  <button 
+    @click="toggleHint" 
+    class="flex items-center cursor-pointer ml-4 text-blue-900 font-semibold p-2 rounded">
+    <img 
+      src="../assets/icons/light-bulp.png" 
+      alt="Hint" 
+      class="mr-1 mb-2 w-5 h-5"
+    >
+    Hint ({{ remainingHints }})
+  </button>
+</template>
